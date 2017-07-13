105e7f503ae8222174adeaca4bbc7f4c658ac240
Ditas <ditasakins@gmail.com>
2017-07-13 15:35:30 -0700

    images, audio, variables

---
+++ assets/javascript/app.js
@@ -0,0 +1,77 @@
+
+
+var questionArr = [ "What was the name of Ned Stark's sword?",
+"How did Daenerys Targaryan acquire three dragon eggs?",
+"How many times has Beric Dondarrion been brought back to life by Thoros of Myr?",
+"What can kill a White Walker besides dragonglass?",
+"What is the only thing that can put out Wildfire?",
+"What does the phrase 'Valar Morghulis' mean?",
+"How many direwolves were in the House Stark?",
+"What was Arya's punishment for stealing from the Many-Face God?",
+"How did King Joffrey finally meet his end?",
+"What was the name of Lady Lyanna Stark's child?",
+"How did Bran lose the use of his leg?",
+"Why does Tyrion's family hate him so much?"];
+
+var answerArr = [ ["Blackfyre", "Ice", "Oathkeeper", "Heartsbane", "Lightbringer"],
+["Found them buried in Essos", "Stolen from the Dothraki", "Gifted to her as a wedding gift",
+"Found in the basement of House Targaryan", "She inherited them from her brother, Viserys"],
+["4 times", "5 times", "6 times", "7 times", "Too many to count"],
+["Weirwood", "Wildfire", "Valyrian Steel", "fire", "water"],
+["water", "sand", "sunlight", "dragon blood", "snow"],
+["All men must die", "All men must serve", "All men must live", "All men must kneel", "All men must drink"],
+["3 wolves", "4 wolves", "5 wolves", "6 wolves", "7 wolves"],
+["death", "memory loss", "blindness", "poverty", "starvation"],
+["stabbed", "hung", "flayed", "poisoned", "drowned"],
+["Rickon", "Salladhor", "Illyrio", "Gendry", "Jon"],
+["Fell out of a tree", "Pushed out a window", "Cut down with a sword", "Born that way", "Fell off a horse"],
+["He is next in line for the throne", "He has a successful business", "He is serving in the Kingsguard",
+"He killed his mother during childbirth", "He is the richest man in Westeros"]];
+
+var correctAnswer = [ "Ice", "Gifted to her as a wedding gift", "6 times", "Valyrian Steel", "sand", "All men must dies",
+"6 wolves", "blindness", "poisoned", "Jon", "Pushed out a window", "He killed his mother during childbirth"];
+
+var correct = 0;
+var incorrect = 0;
+var unanswered = 0;
+
+
+
+
+
+
+
+
+
+$(document).ready(function() {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
